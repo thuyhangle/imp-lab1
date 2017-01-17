f4336b398ecb82709d4ace3bd6f0d277b66d4002
Le Thuy Hang <thuyhangle.le@gmail.com>
2017-01-17 08:43:52 +0200

    Init commit

---
+++ lab1.3/main.js
@@ -0,0 +1,33 @@
+//Change background color to Green
+document.getElementById("green").onmouseover = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#C5DE8E";
+};
+
+document.getElementById("green").onmouseout = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#EC5B68";
+};
+
+//Change background color to Grey
+document.getElementById("grey").onmouseover = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#64605F";
+};
+
+document.getElementById("grey").onmouseout = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#EC5B68";
+};
+
+//Change background color to Pink
+document.getElementById("pink").onmouseover = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#DBABB8";
+};
+
+document.getElementById("pink").onmouseout = function()
+{
+  document.getElementById("myPage").style.backgroundColor = "#EC5B68";
+};
+
---
+++ lab1.6/hello.js
@@ -0,0 +1,36 @@
+console.log("\n\n");
+console.log("H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!");
+console.log("H   H E     L     L     O   O      W W W O   O R   R L     D   D !!");
+console.log("HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !!");
+console.log("H   H E     L     L     O   O  ,,   W W  O   O R   R L     D   D   ");
+console.log("H   H EEEEE LLLLL LLLLL  OOO  ,,    W W   OOO  R   R LLLLL DDDD  !!");
+console.log("\n");
+
+// Taken from: https://en.wikipedia.org/wiki/ASCII_art
+
+/* To run this simple hello world:
+ * 1. You need to install node.js environment to your pc. See instructions
+ *    from here: https://nodejs.org/en/
+ * 2. Copy this file to a folder on your computer.
+ * 3. Open terminal / command prompt
+ * 4. Run a command: node hello.js
+*/
+
+// apple@Macs-Air-3 ~ $ node -v
+// v6.3.1
+// apple@Macs-Air-3 ~ $ cd vagrant
+// apple@Macs-Air-3 ~/vagrant $ cd Multimedia
+// apple@Macs-Air-3 ~/vagrant/Multimedia $ cd Lab1
+// apple@Macs-Air-3 ~/vagrant/Multimedia/Lab1 $ cd lab1.6
+// apple@Macs-Air-3 ~/vagrant/Multimedia/Lab1/lab1.6 $ node hello.js
+
+
+
+// H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!
+// H   H E     L     L     O   O      W W W O   O R   R L     D   D !!
+// HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !!
+// H   H E     L     L     O   O  ,,   W W  O   O R   R L     D   D   
+// H   H EEEEE LLLLL LLLLL  OOO  ,,    W W   OOO  R   R LLLLL DDDD  !!
+
+
+// apple@Macs-Air-3 ~/vagrant/Multimedia/Lab1/lab1.6 $ 
